7478e8b864e62b77bf40099fe33328270c9ff617
Steph <stepsen.dev@gmail.com>
2022-01-31 11:37:16 +0000

    adds Hero component and images

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,15 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+
 import Header from './Header';
 import Footer from './Footer';
-
+import Showcase from "./Showcase";
 // styles
 import styles from "@/styles/Layout.module.css";
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
+
   return (
     <div>
       {/* displays title, meta tags for html generation */}
@@ -15,6 +19,7 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name='keywords' content={keywords} />
       </Head>
       <Header />
+      {router.pathname === '/' && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
