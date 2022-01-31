191380837c50571eba72aa8563f64f99f9b97c6d
Steph <stepsen.dev@gmail.com>
2022-01-31 12:08:57 +0000

    getStaticProps in dev env

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
 import Layout from "/components/Layout";
 import Head from 'next/head';
 
-export default function Home() {
+import { API_URL } from '@/config/index';
+
+export default function Home({ events }) {
   return (
     <div>
       {/* <Head> <title> DJ Events </title></Head> */}
       <Layout>
-        <h1> DJ Events </h1>
+        <h1> Upcoming events </h1>
       </Layout>
     </div>
   );
+}
+
+// this would log server side - runs everytime we come to the page
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}/api/events`);
+//   const events = await res.json()
+
+//   return {
+//     props: { events },
+//     revalidate: 1
+//   }
+// }
+
+// fetching at build time
+// drawback: generated at build time
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json()
+
+  return {
+    props: { events },
+    revalidate: 1 // revalidates after a certain number of seconds
+  }
 }
