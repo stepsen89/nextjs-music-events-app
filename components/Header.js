8e59b10771c429731d484b74ff89e945fa9aaeb8
Steph <stepsen.dev@gmail.com>
2022-02-02 10:22:30 +0000

    starting auth work

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,15 @@
-import Link from 'next/link';
-import styles from '@/styles/Header.module.css'
-import Search from './Search';
+import Link from "next/link";
+import styles from "@/styles/Header.module.css";
+import Search from "./Search";
+
+import AuthContext from "@/context/AuthContext";
+import { useContext } from "react";
+
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -18,13 +25,42 @@ export default function Header() {
               <a> Events </a>
             </Link>
           </li>
-          <li>
-            <Link href="/events/add">
-              <a> Add Event </a>
-            </Link>
-          </li>
+          {user ? (
+            // if logged in
+            <>
+              <li>
+                <Link href="/events/add">
+                  <a> Add Event </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/account/dashboard">
+                  <a> Dashboard </a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  className="btn-secondary btn-icon"
+                  onClick={() => logout()}
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            //if logged out
+            <>
+              <li>
+                <Link href="/account/login">
+                  <a className="btn-secondary btn-icon">
+                    <FaSignInAlt /> Login{" "}
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
-  )
+  );
 }
