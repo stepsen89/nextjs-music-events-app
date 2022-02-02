b1c2540a1aae1da9839db85e1a28ec4dd8c25475
Steph <stepsen.dev@gmail.com>
2022-02-02 13:53:11 +0000

    events/me endpoint to get users events

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,30 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import { parseCookies } from "@/helpers/index";
 
-export default function DashboardPage() {
+export default function DashboardPage({ events }) {
+  console.log(events);
   return (
     <Layout title="User Dashboard">
       <h1>Dashboard</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const data = await res.json();
+  const events = data.data.attributes?.results;
+
+  return {
+    props: { events: events },
+  };
 }
